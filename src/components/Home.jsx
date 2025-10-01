06a0d05cea2b5cf3e31dd3c84862585713585b60
Devirus6341 <devirus101@gmail.com>
2025-10-01 11:58:26 +0100

    updated app.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,64 +1,78 @@
-import { Link, Navigate } from "react-router-dom"
+import { Link } from "react-router-dom";
 import axios from 'axios';
-import { useContext, useState } from "react";
-import { useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../UserContext";
-import '../styles/home.css'
+import '../styles/home.css';
 
- function Home({logout}) {
-  const [user] = useContext(UserContext)
-const [posts, setPosts] = useState ([]);
+function Home({ logout }) {
+  const [user] = useContext(UserContext);
+  const [posts, setPosts] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
     const blogs = async () => {
-      
       try {
-             const response = await axios.get('https://blog-backend-r0rj.onrender.com/posts', {withCredentials: true})
-              const data = response.data;
-           setPosts(data.posts);
+        const response = await axios.get(
+          'https://blog-backend-r0rj.onrender.com/posts',
+          { withCredentials: true }
+        );
+        setPosts(response.data.posts);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     blogs();
-  }, []); 
+  }, []);
 
-  if (!user?.accessToken) {
-  return <Navigate to='/login' replace/>
- }
-
-  const handleViewPost = async(title) => {
-    await axios.post('https://blog-backend-r0rj.onrender.com/post', {title})
-  }
-
+  const handleViewPost = async (title) => {
+    await axios.post('https://blog-backend-r0rj.onrender.com/post', { title });
+  };
 
   return (
-    < div className="container">
+    <div className="container">
       <header className="blog-header">
         <div className="blog-title">
-          <img className="blog-logo" src="images/My-Logo transparent.png" alt="Blog-logo" />
+          <img
+            className="blog-logo"
+            src="images/My-Logo transparent.png"
+            alt="Blog-logo"
+          />
         </div>
-          <div className="dropdown">
-            <button className="dropbtn">Menu</button>
-            <div  className="dropdown-content">
-              <Link className="dropdown-link" to='/create'>Create Post</Link>
-              <Link className="dropdown-link" to='/user/posts'>View My posts</Link>
-             <button className="dropdown-link" onClick={logout}>Log out</button>
-            </div>
-          </div>
-      </header>
-          <h2>SEE LATEST POSTS</h2>
-<div className="body-container">
-    {posts.map((post, index) => {
-     return <Link to='/post' onClick={() =>handleViewPost(post.title)} className="post-link" key={index} >
-      <h3 className="post-link-title">{post.title}</h3>
-        <p> {post.date}</p>
-     </Link>
-    })}
-</div>
 
+        <div className="dropdown">
+          <button className="dropbtn">Menu</button>
+          <div className="dropdown-content">
+            {user ? (
+              <>
+                <Link className="dropdown-link" to="/create">Create Post</Link>
+                <Link className="dropdown-link" to="/user/posts">View My Posts</Link>
+                <button className="dropdown-link" onClick={logout}>Log out</button>
+              </>
+            ) : (
+              <>
+                <Link className="dropdown-link" to="/login">Login</Link>
+                <Link className="dropdown-link" to="/signup">Sign Up</Link>
+              </>
+            )}
+          </div>
+        </div>
+      </header>
+
+      <h2>SEE LATEST POSTS</h2>
+      <div className="body-container">
+        {posts.map((post, index) => (
+          <Link
+            to="/post"
+            onClick={() => handleViewPost(post.title)}
+            className="post-link"
+            key={index}
+          >
+            <h3 className="post-link-title">{post.title}</h3>
+            <p>{post.date}</p>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
